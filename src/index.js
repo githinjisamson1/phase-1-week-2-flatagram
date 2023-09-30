49ad277274cae311cd04fd665dde0ce2d6e4d9c7
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-30 11:59:55 +0300

    Fetch API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 // write your code here
+
+function handleDOMContentLoaded() {
+  // grab elements
+  const cardTitle = document.querySelector("#card-title");
+  const cardImage = document.querySelector("#card-image");
+  let likeCount = document.querySelector("#like-count");
+  const likeButton = document.querySelector("#like-button");
+  const commentsList = document.querySelector("#comments-list");
+  const commentForm = document.querySelector("#comment-form");
+  const comment = document.querySelector("#comment");
+
+  //   set initial likeCount to 0
+  let count = 0;
+
+  //   fetch API - 1
+  fetch("http://localhost:3000/images/1")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      cardTitle.textContent = data.title;
+      cardImage.src = data.image;
+
+     
+    });
+}
+
+document.addEventListener("DOMContentLoaded", handleDOMContentLoaded);
